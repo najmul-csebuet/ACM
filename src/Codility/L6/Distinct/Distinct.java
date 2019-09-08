44062cd74f18d64c4dedd771d6592c85c767ad2a
Md. Najmul Hasan <najmul@qianalysis.com>
2019-09-08 14:11:59 +0600

    AC

---
+++ src/Codility/L6/Distinct/Distinct.java
@@ -0,0 +1,95 @@
+package Codility.L6.Distinct;
+
+import java.io.*;
+import java.util.*;
+
+public class Distinct {
+
+    public static void main(String[] args) throws IOException {
+
+        boolean fileInOut = Distinct.class.getPackage() != null;
+
+        Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(fileInOut ? Distinct.class.getResourceAsStream("in.txt") : System.in)));
+        Solution.out = new MyPrintWriter(new BufferedOutputStream(fileInOut ? new FileOutputStream("out.txt") : System.out), true);
+
+        int testCase = fileInOut ? sc.nextInt() : 1;
+
+        for (int i = 0; i < testCase; i++) {
+            new Solution().solution();
+        }
+
+        if (fileInOut) {
+
+            verify(Distinct.class.getResource("ans.txt").getFile());
+        }
+    }
+
+    public static void verify(String ansFile) throws IOException {
+
+        String outputFile = "out.txt";
+
+        BufferedReader reader1 = new BufferedReader(new FileReader(ansFile));
+        BufferedReader reader2 = new BufferedReader(new FileReader(outputFile));
+
+        String line1 = reader1.readLine();
+        String line2 = reader2.readLine();
+
+        boolean areEqual = true;
+        int lineNum = 1;
+
+        while (line1 != null || line2 != null) {
+
+            if (line1 == null || line2 == null) {
+
+                areEqual = false;
+                break;
+            } else if (!line1.equals(line2)) {
+
+                areEqual = false;
+                break;
+            }
+
+            line1 = reader1.readLine();
+            line2 = reader2.readLine();
+
+            lineNum++;
+        }
+
+        if (areEqual) {
+
+            System.out.println("All Test Cases Passed !");
+        } else {
+
+            System.out.println("Output differ at line " + lineNum);
+            System.out.println("ans.txt has " + line1 + " and out.txt has " + line2 + " at line " + lineNum);
+        }
+
+        reader1.close();
+        reader2.close();
+    }
+}
+
+class MyPrintWriter extends PrintWriter {
+
+    public MyPrintWriter(BufferedOutputStream bufferedOutputStream, boolean autoFlash) {
+        super(bufferedOutputStream, autoFlash);
+    }
+
+    public void print(int[] A) {
+        for (int i = 0; i < A.length - 1; i++) {
+            print(A[i] + ' ');
+        }
+        if (A.length > 0) {
+            println(A[A.length - 1]);
+        }
+    }
+}
+
+class Solution {
+
+    public static MyPrintWriter out;
+
+    public void solution() {
+
+    }
+}
