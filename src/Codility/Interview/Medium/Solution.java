76e6e0572107a46607c1c485e0bfbcaa60bd6147
najmul.csebuet <najmul.csebuet@gmail.com>
2020-07-19 10:29:31 +0600

    Toptal

--- src/Codility/Interview/Medium/Solution.java
+++ src/Codility/Interview/Medium/Solution.java
@@ -10,8 +10,9 @@ public class Solution {
         Solution s = new Solution();
         ArrayList<Boolean> l = new ArrayList<>();
 
-        l.add(s.solution(new int[]{}) == 0);
-        //l.add(Arrays.equals(s.solution(new int[]{}), new int[] {}));
+        l.add(s.solution("ABBBCCDDCCC", 3) == 5);
+        l.add(s.solution("AAAAAAAAAAABXXAAAAAAAAAA", 3) == 3);
+        l.add(s.solution("ABCDDDEFG", 2) == 6);
 
         checkTestCases(l);
     }
@@ -29,8 +30,84 @@ public class Solution {
         System.out.println(allPassed ? "All Test Cases Passed!" : "");
     }
 
-    public int solution(int[] A) {
+    String compress(String S) {
 
-        return 0;
+        char[] charArray = S.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < charArray.length; i++) {
+            char ch = charArray[i];
+            int count = 1;
+            int j = i + 1;
+            for (; j < charArray.length; j++) {
+                if (ch == charArray[j]) {
+                    ++count;
+                } else {
+                    break;
+                }
+            }
+            i = j - 1;
+
+            if (count == 1) {
+                sb.append(ch);
+            } else {
+                sb.append(new Integer(count).toString() + ch);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    String compress(String S, int s, int e) {
+
+        char[] charArray = S.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < charArray.length; i++) {
+            if (s <= i && i <= e) continue;
+            char ch = charArray[i];
+            int count = 1;
+            int j = i + 1;
+            for (; j < charArray.length; j++) {
+                if (s <= j && j <= e) continue;
+                if (ch == charArray[j]) {
+                    ++count;
+                } else {
+                    break;
+                }
+            }
+            i = j - 1;
+
+            if (count == 1) {
+                sb.append(ch);
+            } else {
+                sb.append(Integer.toString(count) + ch);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public int solution(String S, int K) {
+
+        if (K + 1 == S.length()) {
+            return 1;
+        }
+
+        if (K == 0) {
+            return compress(S).length();
+        }
+
+        int min = Integer.MAX_VALUE;
+        StringBuilder sb = new StringBuilder(S);
+
+        for (int i = 0; i < S.length() - K; i++) {
+//            StringBuilder replace = sb.replace(i, i + K, "");
+//            String compressed = compress(replace.toString());
+            String compressed = compress(sb.toString(), i, i + K - 1);
+            min = Math.min(min, compressed.length());
+        }
+        // write your code in Java SE 8
+        return min;
     }
 }
