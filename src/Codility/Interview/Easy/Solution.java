983c5a83271a91ab84e86b3f520a4f78ee065e0a
Md. Najmul Hasan <najmul@qianalysis.com>
2020-07-19 08:52:49 +0600

    Toptal

--- src/Codility/Interview/Easy/Solution.java
+++ src/Codility/Interview/Easy/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
         Solution s = new Solution();
         ArrayList<Boolean> l = new ArrayList<>();
 
-        l.add(s.solution("ABDCA", new int[]{2,-1,-4,-3,3}, new int[]{2,-2,4,1,-3}) == 0);
+        l.add(s.solution("ABDCA", new int[]{2,-1,-4,-3,3}, new int[]{2,-2,4,1,-3}) == 3);
         //l.add(Arrays.equals(s.solution(new int[]{}), new int[] {}));
 
         checkTestCases(l);
@@ -29,7 +29,34 @@ public class Solution {
         System.out.println(allPassed ? "All Test Cases Passed!" : "");
     }
 
+    class Point {
+
+        char label = 'A';
+        int x,y;
+        double dist;
+
+        public Point(char label, int x, int y) {
+            this.label = label;
+            this.x = x;
+            this.y = y;
+            dist = Math.sqrt(1.0*x*x + 1.0*y*y);
+        }
+    }
+
     public int solution(String S, int[] X, int[] Y) {
         // write your code in Java SE 8
+        ArrayList<Point> pointList = new ArrayList<>();
+
+        for (int i = 0; i < X.length; i++) {
+            Point p = new Point(S.charAt(i), X[i], Y[i]);
+            pointList.add(p);
+        }
+
+        pointList.sort((a,b) -> {
+            if (a.dist <= b.dist)return -1;
+            return 1;
+        });
+
+
     }
 }
