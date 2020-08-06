1218509260e48f5b6b959ce2599175c0566a3f1a
najmul.csebuet <najmul.csebuet@gmail.com>
2020-08-06 09:35:24 +0600

    AC

--- src/Leetcode/Practice/Easy/P278.java
+++ src/Leetcode/Practice/Easy/P278.java
@@ -23,29 +23,32 @@ public class P278 {
         P278 s = new P278();
         ArrayList<Boolean> l = new ArrayList<>();
 
-        //l.add(s.firstBadVersion() == 0);
-        //l.add(Arrays.equals(s.solution(new int[]{}), new int[] {}));
+        l.add(s.firstBadVersion(10) == 6);
 
         checkTestCases(l);
     }
 
     public int firstBadVersion(int n) {
 
-        int firstBadVersion = 0;
+        int firstBadVersion = Integer.MAX_VALUE;
 
         int low = 1, high = n;
         while (low <= high) {
-            int mid = (low + high) >> 1;
-            if (isBadVersion(mid) != true) {
+            //int mid = (low + high) >> 1;
+            int mid = low + ((high - low) / 2);
+            if (isBadVersion(mid)) {
+                high = mid - 1;
                 firstBadVersion = Math.min(firstBadVersion, mid);
+            } else {
+                low = mid + 1;
             }
-            high = mid - 1;
         }
 
         return firstBadVersion;
     }
 
     private boolean isBadVersion(int mid) {
-        return false;
+        boolean[] booleans = {false, false, false, false, false, false, true, true, true, true, true};
+        return booleans[mid];
     }
 }
