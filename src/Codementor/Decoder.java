1c91cce1c9884bfa8f5f25ea45c56239efd7a50a
najmul.csebuet <najmul.csebuet@gmail.com>
2020-10-12 12:37:49 +0600

    Decoder version 2

--- src/Codementor/Decoder.java
+++ src/Codementor/Decoder.java
@@ -8,10 +8,12 @@ public class Decoder {
     public static Scanner sc;
     public static PrintWriter out;
 
+    private int[] rs;
+
     public static void main(String[] args) throws IOException {
 
         boolean fileInOut = !Decoder.class.getPackage().getName().isEmpty();
-        sc = new Scanner(new BufferedReader(new InputStreamReader(fileInOut ? Decoder.class.getResourceAsStream("in1.txt") : System.in)));
+        sc = new Scanner(new BufferedReader(new InputStreamReader(fileInOut ? Decoder.class.getResourceAsStream("in.txt") : System.in)));
         out = new PrintWriter(new BufferedOutputStream(fileInOut ? new FileOutputStream("out.txt") : System.out), true);
 
         int totalTC = fileInOut ? sc.nextInt() : 1;
@@ -21,56 +23,81 @@ public class Decoder {
             int M = sc.nextInt();
 
             int[] encryptedText = new int[N];
-            for (int i = 0; i < encryptedText.length; ++i)encryptedText[i] = sc.nextInt();
+            for (int i = 0; i < encryptedText.length; ++i) encryptedText[i] = sc.nextInt();
 
             int[] plainText = new int[M];
-            for (int i = 0; i < plainText.length; ++i)plainText[i] = sc.nextInt();
+            for (int i = 0; i < plainText.length; ++i) plainText[i] = sc.nextInt();
 
             new Decoder().solution(N, M, encryptedText, plainText);
         }
     }
 
-    private int[] getRs(int A, int B, int M, int r0, int n) {
-        var rs = new int[n+1];
-        rs[0] = r0;
-        for (int i = 1; i <= n; i++) {
-            rs[i] = (A*rs[i-1] + B) % M;
+    private int[] getRs(int A, int B, int M, int n, int m) {
+        for (int i = m + 1; i <= n; i++) {
+            rs[i] = (A * rs[i - 1] + B) % M;
         }
         return rs;
     }
 
     public void solution(int n, int m, int[] encryptedText, int[] plainText) {
-        for (int M = 0; M < 1000; M++) {
+
+        rs = new int[n + 1];
+        for (int i = 0; i < m; i++) {
+            rs[i + 1] = encryptedText[i] ^ plainText[i];
+        }
+
+        for (int M = 1; M < 1000; M++) {
             for (int B = 0; B < M; B++) {
-                for (int r0 = 0; r0 < M; r0++) {
-                    for (int A = 0; A < M; A++) {
-                        //We have A, B, M, r0
-                        int[] rs = getRs(A, B, M, r0, n);
-                        int[] candidatePlainText = new int[n];
-                        for (int i = 0; i < n; i++) {
-                            candidatePlainText[i] = encryptedText[i] ^ rs[i + 1];
-                        }
 
-                        boolean flag = true;
-
-                        for (int i = 0; i < m; i++) {
-                            if (plainText[i] == candidatePlainText[i])continue;
-                            flag = false;
-                            break;
-                        }
-
-                        if (flag) {
-                            System.out.println((A%M) + " " + (B%M) + " " + M + " " + (r0%M));
-                            for (int i = 0; i < n - 1; i++) {
-                                System.out.print(candidatePlainText[i] + " ");
-                            }
-
-                            if (n > 0) {
-                                System.out.println(candidatePlainText[n - 1]);
-                            }
-                            return;
-                        }
+                int A;
+                for (A = 0; A < 1000; A++) {
+                    int t = (A * rs[1] + B) % M;
+                    if (t == rs[2]) {
+                        break;
                     }
+                }
+
+                if (A == 1000) {
+                    continue;
+                }
+
+                int r0;
+                for (r0 = 0; r0 < 1000; r0++) {
+                    //r2 = (A * r1 + B) % M
+                    int t = (A * r0 + B) % M;
+                    if (t == rs[1]) {
+                        rs[0] = r0;
+                        break;
+                    }
+                }
+                if (r0 == 1000) {
+                    continue;
+                }
+                //We have A, B, M, r0
+                getRs(A, B, M, n, m);
+                int[] candidatePlainText = new int[n];
+                for (int i = 0; i < n; i++) {
+                    candidatePlainText[i] = encryptedText[i] ^ rs[i + 1];
+                }
+
+                boolean flag = true;
+
+                for (int i = 0; i < m; i++) {
+                    if (plainText[i] == candidatePlainText[i]) continue;
+                    flag = false;
+                    break;
+                }
+
+                if (flag) {
+                    System.out.println((A % M) + " " + (B % M) + " " + M + " " + (r0 % M));
+                    for (int i = 0; i < n - 1; i++) {
+                        System.out.print(candidatePlainText[i] + " ");
+                    }
+
+                    if (n > 0) {
+                        System.out.println(candidatePlainText[n - 1]);
+                    }
+                    return;
                 }
             }
         }
